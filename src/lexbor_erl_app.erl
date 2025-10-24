2a1f5431786fcd4355a9d952ea326211126a1bcd
Frank Eickhoff <info@frankeickhoff.de>
2025-10-24 02:25:21 +0200

    Start pool in application

--- src/lexbor_erl_app.erl
+++ src/lexbor_erl_app.erl
@@ -4,7 +4,10 @@
 -export([start/2, stop/1]).
 
 start(_Type, _Args) ->
-    lexbor_erl_port:start_link().
+    % Get pool size from configuration, default to number of schedulers
+    PoolSize = application:get_env(lexbor_erl, pool_size, 
+                                   erlang:system_info(schedulers_online)),
+    lexbor_erl_sup:start_link(PoolSize).
 
 stop(_State) ->
     ok.
